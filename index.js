9ee09f5e7cb7d322938d10a1e0efb3efa8ea974f
Abhishek Jha <abhishekjha3012@gmail.com>
2020-03-28 04:59:09 +0530

    updated logic for timer
    updated logic for timer

--- index.js
+++ index.js
@@ -2,13 +2,14 @@
 const triggerTimer = () => {
     const present = new Date();
     const end = new Date(2020, 3, 14, 23, 59, 59);
-    const diffTime = Math.abs(end - present);
-    const diffDays = Math.floor(diffTime / (1000 * 60 * 60 * 24)); 
-    const diffHours = Math.floor((diffTime / (1000 * 60 * 60 )) % 60); 
-    const diffMinute= Math.floor((diffTime / (1000 * 60)) % 60 ); 
-    const diffSecond = Math.floor((diffTime / 1000 ) % 60)
+    const diffTime = Math.abs(end - present)/1000;
+    const diffDays = Math.floor(diffTime / ( 36e2 * 24)); 
+    const diffHours = Math.floor((diffTime / 36e2) % 24 ); 
+    const diffMinute= Math.floor((diffTime / 60) % 60 ); 
+    const diffSecond = Math.floor(diffTime % 60);
     const timerHtml = `${diffDays} days : ${diffHours} hrs : ${diffMinute} minutes : ${diffSecond} seconds`;
     document.getElementsByClassName('time-left')[0].innerHTML = timerHtml;  
+    console.log(present, end)
 }
 
 const domLoaded = () => {
