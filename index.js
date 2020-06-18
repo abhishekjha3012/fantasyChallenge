ae4ea21c919427476617667de6a61649ce6311a9
Abhishek Jha <abhishekjha3012@gmail.com>
2020-06-19 01:26:21 +0530

    added detail tab
    added detail tab

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const tableUrl = 'https://api.covid19india.org/v3/min/data.min.json';
 const newsUrl = ' https://api.covid19india.org/updatelog/log.json';
 
+let rowData = [];
 const STATE_NAMES = {
     AP: 'Andhra Pradesh',
     AR: 'Arunachal Pradesh',
@@ -48,13 +49,16 @@ const defaultColDef = {
 };
 
 const columnDefs = [
-    {headerName: "State/UT", field: "state", width: '180'},
+    {headerName: "State/UT", field: "state", width: '100'},
     {headerName: "Confirmed", field: "confirmed", sort:'desc'},
     {headerName: "Active (%) ", field: "active"},
     {headerName: "Recovered(%)", field: "recovered"},
-    {headerName: "Deceased (%)", field: "deceased" },
-    
+    {headerName: "Deceased (%)", field: "deceased" },    
 ];
+
+const onFirstDataRendered = (params) => {
+    params.api.sizeColumnsToFit();
+}
 
 const fetchNewsData = () => {
     return fetch(newsUrl)
@@ -89,27 +93,43 @@ const formatTableData = data => {
     return formattedRowData;
 };
 
+const paintDetail = () => {
+    const detailData = rowData['TT'];
+    const { confirmed=0, recovered=0, deceased=0, tested=0, migrated=0 } = detailData.total;
+    const active = confirmed-recovered-deceased;
+
+    document.querySelector('.detail-confirmed .count').innerHTML = confirmed;
+    document.querySelector('.detail-active .count').innerHTML = active;
+    document.querySelector('.detail-recovered .count').innerHTML = recovered;
+    document.querySelector('.detail-deceased .count').innerHTML = deceased;
+    document.querySelector('.detail-tested .count').innerHTML = tested;
+    document.querySelector('.detail-migrated .count').innerHTML = migrated;
+    
+}
+
 const paintNews = async () => {
     let newsData = await fetchNewsData();
-    const newsHtml = newsData.map(item => (
+    const newsHtml = newsData.map((item, index) => (
         `<div class='news-box'>
-            <p class='update'>${item.update}</p>
+            <p class='update'>${index+1}. ${item.update}</p>
             <p class='time'>${moment(item.timestamp).format('DD/MM/YYYY hh:mm a')}</p>
         </div>`
     ));
-    document.querySelector('#news .news-body').innerHTML= newsHtml.join('');
+    document.querySelector('.news-body').innerHTML= newsHtml.join('');
 };
 
 const paintTable = async () => {
-    const rowData = await fetchTableData();
-    const formattedRowData= formatTableData(rowData);   
-    const gridDiv = document.querySelector('#myGrid');
+    rowData = await fetchTableData();
+    const formattedRowData = formatTableData(rowData);   
+    const gridDiv = document.querySelector('.data-table');
     const gridOptions = {
         columnDefs,
         defaultColDef,
         animateRows:true,
         postSort: postSort(),
-        rowData: formattedRowData
+        rowClass: 'custom-row-class',
+        rowData: formattedRowData,
+        onFirstDataRendered,
     };
     new agGrid.Grid(gridDiv, gridOptions);
 };
@@ -118,7 +138,8 @@ const postSort = rowNodes => {
     console.log(rowNodes);
 };
     
-document.addEventListener('DOMContentLoaded', function() {
-    paintNews();
-    paintTable();    
+document.addEventListener('DOMContentLoaded', async () => {    
+    await paintTable();  
+    paintNews(); 
+    paintDetail();
 });
