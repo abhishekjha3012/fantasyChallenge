c542ebae06feb32f1a6db4e5ab9fe86d52b0d258
Abhishek Jha <abhishekjha3012@gmail.com>
2022-04-09 02:18:30 +0530

    added audio

--- index.js
+++ index.js
@@ -449,6 +449,17 @@ const triggerButtonSelection = node => {
     }
 }
 
+const triggershoutOut = () => {
+    const audioElement = document.querySelector('.shout-out-audio');
+    const lastMatchRecord = masterData.pop().result;
+    const rankObject = Object.fromEntries(Object.entries(lastMatchRecord).map(a => {
+        a[1] = Math.abs(a[1]);
+        return a.reverse();
+    }));
+    audioElement.src = `asset/A${rankObject[1]}.mp3`;
+    audioElement.play();
+
+}
 const domLoaded = () => {
     document.querySelector('.loading-msg').style.display = 'block';
     document.querySelector('.rank-table').style.display = 'none';
